a032e3d4789c84ecef76dc7f3353faf087c77cd2
Olexander Zhovanyk <mindesync@gmail.com>
2024-05-14 14:55:15 +0300

    chore: Update dependencies and file paths

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const ArialPath = join(__dirname, "Arial.ttf");
 
